57793df21f026f8e4524d5726db58c454fa14b17
Fela Maslen <felamaslen@gmail.com>
2017-10-21 13:18:14 +0100

    Draw line on canvas

--- web/src/containers/Draw/index.js
+++ web/src/containers/Draw/index.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const COLOR_DRAW = 'rgb(0, 0, 0)';
+const LINE_WIDTH = 2;
+
 export class Draw extends Component {
     constructor(props) {
         super(props);
@@ -11,13 +14,92 @@ export class Draw extends Component {
         this.ctx = null;
         this.width = 0;
         this.height = 0;
+
+        this.drawing = false;
+
+        this.state = {
+            lines: []
+        };
+    }
+    onDrawStart({ posX, posY }) {
+        this.drawing = true;
+
+        this.setState({
+            lines: [[[posX, posY]], ...this.state.lines]
+        });
+    }
+    onDraw({ posX, posY }) {
+        if (!this.drawing) {
+            return;
+        }
+
+        this.setState({
+            lines: [
+                [
+                    ...this.state.lines[0],
+                    [posX, posY]
+                ],
+                ...this.state.lines.slice(1)
+            ]
+        });
+    }
+    onDrawEnd({ posX, posY }) {
+        this.onDraw({ posX, posY });
+
+        this.drawing = false;
+    }
+    draw() {
+        // draw the current line on the canvas
+        if (!this.ctx) {
+            return;
+        }
+
+        this.ctx.clearRect(0, 0, this.width, this.height);
+
+        this.ctx.beginPath();
+
+        this.state.lines.forEach(line => {
+            line.forEach((point, key) => {
+                if (key > 0) {
+                    this.ctx.lineTo(point[0], point[1]);
+                }
+                else {
+                    this.ctx.moveTo(point[0], point[1]);
+                }
+            });
+        });
+
+        this.ctx.strokeStyle = COLOR_DRAW;
+        this.ctx.lineWidth = LINE_WIDTH;
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+    componentDidMount() {
+        this.draw();
+    }
+    componentDidUpdate() {
+        this.draw();
+    }
+    getPosXY(evt) {
+        if (!this.canvas) {
+            return null;
+        }
+
+        return {
+            posX: evt.clientX - this.canvas.offsetLeft,
+            posY: evt.clientY - this.canvas.offsetTop
+        };
     }
     render() {
         const canvasRef = canvas => {
+            if (!(canvas && canvas.parentNode)) {
+                return;
+            }
+
             this.canvas = canvas;
 
-            this.canvas.width = canvas.parentNode.innerWidth;
-            this.canvas.height = canvas.parentNode.innerHeight;
+            this.canvas.width = canvas.parentNode.offsetWidth - 2;
+            this.canvas.height = canvas.parentNode.offsetHeight - 2;
 
             this.width = this.canvas.width;
             this.height = this.canvas.height;
@@ -25,8 +107,28 @@ export class Draw extends Component {
             this.ctx = canvas.getContext('2d');
         };
 
+        const onMouseDown = evt => this.onDrawStart(this.getPosXY(evt));
+        const onMouseUp = evt => this.onDrawEnd(this.getPosXY(evt));
+        const onMouseMove = evt => this.onDraw(this.getPosXY(evt));
+        const onMouseOut = onMouseUp;
+        const onTouchStart = onMouseDown;
+        const onTouchMove = onMouseMove;
+        const onTouchEnd = onMouseUp;
+
         return <div className="draw-outer">
-            <canvas className="draw-canvas" ref={canvasRef} />
+            <div className="draw-canvas-outer">
+                <canvas
+                    className="draw-canvas"
+                    ref={canvasRef}
+                    onMouseDown={onMouseDown}
+                    onMouseUp={onMouseUp}
+                    onMouseOut={onMouseOut}
+                    onMouseMove={onMouseMove}
+                    onTouchStart={onTouchStart}
+                    onTouchMove={onTouchMove}
+                    onTouchEnd={onTouchEnd}
+                />
+            </div>
         </div>;
     }
 }
